d635ad42c02ac96699d6e77703be7c46c935818f
Gregg Mojica <gwmojica@gmail.com>
2015-09-28 15:49:58 -0400

    Initial Commit

---
+++ AppThinning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppThinning
+//
+//  Created by Gregg Mojica on 9/28/15.
+//  Copyright © 2015 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
